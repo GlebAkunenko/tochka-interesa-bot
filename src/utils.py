573925892fa00c0b5b1de35a6934d78fe4565259
Gleb Akunenko <akunenko.study@gmail.com>
2024-03-29 23:50:16 +0300

    ?

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from pyteledantic.models import MessageToSend, Message
 
 import pyteledantic.methods as tg
-import aiohttp, asyncio
+import aiohttp, asyncio, requests
 
 from src.model import Moderator
 from src.config import token
@@ -11,11 +11,11 @@ async def notify_moderators(text: str):
     moderators: list[Moderator] = Moderator.select()
     if moderators:
         url = f'https://api.telegram.org/bot{token}/sendMessage'
-        async with aiohttp.ClientSession() as session:
+        with requests.Session() as session:
             tasks = []
             for moderator in moderators:
                 message = MessageToSend(chat_id=moderator.telegram_id, text=text, parse_mode="MarkdownV2")
-                await session.request('GET', url, params=message.dict())
+                session.request('GET', url, params=message.dict())
             # await asyncio.wait(tasks)
 
 
